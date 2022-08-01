dc6c52eb9fff3b4e51944e8bfe4f5a8eb3fd3053
JooSeongWon <92145089+JooSeongWon@users.noreply.github.com>
2022-08-01 14:18:13 +0900

    calendar 기본

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 import Calendar from "./Calendar";
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import NotFound from "./NotFound";
-import Date from "./Date";
 
 function App() {
   return (
@@ -11,8 +9,6 @@ function App() {
         <Routes>
           {/* path경로에 따라 다른 컴포넌트 호출 */}
           <Route path="/" element={<Calendar />}></Route> 
-          <Route path="/date/:id" element={<Date />}></Route>
-          <Route path="*" element={<NotFound />}></Route>
         </Routes>
       </BrowserRouter>
     </div>
